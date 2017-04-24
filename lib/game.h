9290ffdca36933498ab8ad72f6959d2ee710c51f
Baptiste Gerondeau <baptiste@gerondeau.net>
2017-04-24 04:56:15 +0200

    commentaires a foison

--- lib/game.h
+++ lib/game.h
@@ -14,7 +14,9 @@ class game{
           game (vector <vector <char> > board);
 
         //PUBLIC METHODS
-        vector <vector <int> >  valid_move(int x, int y, char col); ///Fonction vérifiant que le mouvement est autorisé et retournant l'ensemble des cases a retourner
+
+        ///Fonction vérifiant que le mouvement est autorisé et retournant l'ensemble des cases a retourner
+        vector <vector <int> >  valid_move(int x, int y, char col);
         int Place(int x, int y, char col); ///
         int Getwin();
         void save(char play_col, int gamemode=0, int ia_lvl=0 );
